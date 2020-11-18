b85e4a04fcb733538ae3f216d610a60c7e231abe
Roslyn <roz@axworks.com>
2020-11-18 15:12:09 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,207 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <button
+                type="button"
+                class="navbar-toggle"
+                data-toggle="collapse"
+                data-target="#myNavbar"
+              >
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+              <a class="navbar-brand" href="#">
+                thetinyraver
+              </a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+              <ul class="nav navbar-nav">
+                <li class="active">
+                  <a href="#">Home</a>
+                </li>
+              </ul>
+              <p class="navbar-text">Welcome to thetinyraver!</p>
+              <ul class="nav navbar-nav navbar-right">
+                <li>
+                  <a href="#">
+                    <span class="glyphicon glyphicon-search"></span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="glyphicon glyphicon-user"></span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="glyphicon glyphicon-shopping-cart"></span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div class="jumbotron">
+          <div class="container text-center">
+            <h1>Online Store</h1>
+            <p>Mission, Vission & Values}</p>
+          </div>
+        </div>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="panel panel-primary">
+                <div class="panel-heading">BLACK FRIDAY DEAL</div>
+                <div class="panel-body">
+                  <img
+                    src="https://placehold.it/150x80?text=IMAGE"
+                    class="img-responsive"
+                    style={{ width: "100%" }}
+                    alt="Image"
+                  ></img>
+                </div>
+                <div class="panel-footer">
+                  Buy 50 mobiles and get a gift card
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="panel panel-danger">
+                <div class="panel-heading">BLACK FRIDAY DEAL</div>
+                <div class="panel-body">
+                  <img
+                    src="https://placehold.it/150x80?text=IMAGE"
+                    class="img-responsive"
+                    style={{ width: "100%" }}
+                    alt="Image"
+                  ></img>
+                </div>
+                <div class="panel-footer">
+                  Buy 50 mobiles and get a gift card
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="panel panel-success">
+                <div class="panel-heading">BLACK FRIDAY DEAL</div>
+                <div class="panel-body">
+                  <img
+                    src="https://placehold.it/150x80?text=IMAGE"
+                    class="img-responsive"
+                    style={{ width: "100%" }}
+                    alt="Image"
+                  ></img>
+                </div>
+                <div class="panel-footer">
+                  Buy 50 mobiles and get a gift card
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br></br>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="panel panel-primary">
+                <div class="panel-heading">BLACK FRIDAY DEAL</div>
+                <div class="panel-body">
+                  <img
+                    src="https://placehold.it/150x80?text=IMAGE"
+                    class="img-responsive"
+                    style={{ width: "100%" }}
+                    alt="Image"
+                  ></img>
+                </div>
+                <div class="panel-footer">
+                  Buy 50 mobiles and get a gift card
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="panel panel-primary">
+                <div class="panel-heading">BLACK FRIDAY DEAL</div>
+                <div class="panel-body">
+                  <img
+                    src="https://placehold.it/150x80?text=IMAGE"
+                    class="img-responsive"
+                    style={{ width: "100%" }}
+                    alt="Image"
+                  ></img>
+                </div>
+                <div class="panel-footer">
+                  Buy 50 mobiles and get a gift card
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-4">
+              <div class="panel panel-primary">
+                <div class="panel-heading">BLACK FRIDAY DEAL</div>
+                <div class="panel-body">
+                  <img
+                    src="https://placehold.it/150x80?text=IMAGE"
+                    class="img-responsive"
+                    style={{ width: "100%" }}
+                    alt="Image"
+                  ></img>
+                </div>
+                <div class="panel-footer">
+                  Buy 50 mobiles and get a gift card
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br></br>
+
+        <footer class="container-fluid text-center">
+          <p>Online Store Copyright</p>
+          <form class="form-inline">
+            Get deals:
+            <input
+              type="email"
+              class="form-control"
+              size="50"
+              placeholder="Email Address"
+            ></input>
+            <button type="button" class="btn btn-danger">
+              Sign Up
+            </button>
+          </form>
+        </footer>
+      </div>
+    );
+  }
+}
+
+class Home extends React.Component {
+  // renderSquare(i) {
+  //   return <Square />;
+  // }
+
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Home />, document.getElementById("root"));
